6bc8def2194082fc41348718cc5ac28839d1c8ee
Aris <aris@portcities.net>
2020-08-12 08:18:32 +0700

    update

--- aos_product_dimension_cartoon/models/product.py
+++ aos_product_dimension_cartoon/models/product.py
@@ -11,46 +11,64 @@ class ProductUom(models.Model):
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    item_size_l = fields.Float(string="Item Length")
-    item_size_w = fields.Float(string="Item Width")
-    item_size_h = fields.Float(string="Item Height")
+    item_size_l = fields.Float(string="Item Length", inverse=lambda self:self._inverse_size())
+    item_size_w = fields.Float(string="Item Width", inverse=lambda self:self._inverse_size())
+    item_size_h = fields.Float(string="Item Height", inverse=lambda self:self._inverse_size())
 
 
-    case_pack_size_l = fields.Float(string="Case Pack Length")
-    case_pack_size_w = fields.Float(string="Case Pack Width")
-    case_pack_size_h = fields.Float(string="Case Pack Height")
+    case_pack_size_l = fields.Float(string="Case Pack Length", inverse=lambda self:self._inverse_size())
+    case_pack_size_w = fields.Float(string="Case Pack Width", inverse=lambda self:self._inverse_size())
+    case_pack_size_h = fields.Float(string="Case Pack Height", inverse=lambda self:self._inverse_size())
 
-    qty_per_ctn = fields.Float(string="Qty/CTN")
+    qty_per_ctn = fields.Float(string="Qty/CTN", inverse=lambda self:self._inverse_size())
 
 
     size_unit_id = fields.Many2one('product.uom', string="Size Unit", default=lambda self:self.env.ref('product.product_uom_cm').id, required=True)
 
+
+    def _inverse_size(self):
+        for rec in self:
+            for var in rec.product_variant_ids:
+                var.update({
+                    'item_size_l':rec.item_size_l,
+                    'item_size_h':rec.item_size_h,
+                    'item_size_w':rec.item_size_w,
+
+                    'case_pack_size_l':rec.case_pack_size_l,
+                    'case_pack_size_h':rec.case_pack_size_h,
+                    'case_pack_size_w':rec.case_pack_size_w,
+
+                    'qty_per_ctn':rec.qty_per_ctn,
+
+                })
+
+
 class ProductProduct(models.Model):
     _inherit = 'product.product'
 
-    item_size_l = fields.Float(string="Item Length", related=False, readonly=False, compute="_compute_inventory", inverse=lambda self:self._inverse_inventory(), store=True)
-    item_size_w = fields.Float(string="Item Width", related=False, readonly=False, compute="_compute_inventory", inverse=lambda self:self._inverse_inventory(), store=True)
-    item_size_h = fields.Float(string="Item Height", related=False, readonly=False, compute="_compute_inventory", inverse=lambda self:self._inverse_inventory(), store=True)
+    item_size_l = fields.Float(string="Item Length", store=True)
+    item_size_w = fields.Float(string="Item Width", store=True)
+    item_size_h = fields.Float(string="Item Height", store=True)
 
 
-    case_pack_size_l = fields.Float(string="Case Pack Length", related=False, readonly=False, compute="_compute_inventory", inverse=lambda self:self._inverse_inventory(), store=True)
-    case_pack_size_w = fields.Float(string="Case Pack Width", related=False, readonly=False, compute="_compute_inventory", inverse=lambda self:self._inverse_inventory(), store=True)
-    case_pack_size_h = fields.Float(string="Case Pack Height", related=False, readonly=False, compute="_compute_inventory", inverse=lambda self:self._inverse_inventory(), store=True)
+    case_pack_size_l = fields.Float(string="Case Pack Length", store=True)
+    case_pack_size_w = fields.Float(string="Case Pack Width", store=True)
+    case_pack_size_h = fields.Float(string="Case Pack Height", store=True)
 
-    qty_per_ctn = fields.Float(string="Qty/CTN", related=False, readonly=False, compute="_compute_inventory", inverse=lambda self:self._inverse_inventory(), store=True)
+    qty_per_ctn = fields.Float(string="Qty/CTN", store=True)
 
     size_unit_id = fields.Many2one('product.uom', string="Size Unit", related="product_tmpl_id.size_unit_id", readonly=True)
     
-
-    def _compute_inventory(self):
-        for rec in self:
-            rec.item_size_h = rec.product_tmpl_id.item_size_h
-            rec.item_size_w = rec.product_tmpl_id.item_size_w
-            rec.item_size_l = rec.product_tmpl_id.item_size_l
-            rec.case_pack_size_h = rec.product_tmpl_id.case_pack_size_h
-            rec.case_pack_size_w = rec.product_tmpl_id.case_pack_size_w
-            rec.case_pack_size_l = rec.product_tmpl_id.case_pack_size_l
-            rec.qty_per_ctn = rec.product_tmpl_id.qty_per_ctn
+    # @api.depends('product_tmpl_id.item_size_h','product_tmpl_id.item_size_w','product_tmpl_id.item_size_l','product_tmpl_id.case_pack_size_h','product_tmpl_id.case_pack_size_w','product_tmpl_id.case_pack_size_l','product_tmpl_id.qty_per_ctn')
+    # def _compute_inventory(self):
+    #     for rec in self:
+    #         rec.item_size_h = rec.product_tmpl_id.item_size_h
+    #         rec.item_size_w = rec.product_tmpl_id.item_size_w
+    #         rec.item_size_l = rec.product_tmpl_id.item_size_l
+    #         rec.case_pack_size_h = rec.product_tmpl_id.case_pack_size_h
+    #         rec.case_pack_size_w = rec.product_tmpl_id.case_pack_size_w
+    #         rec.case_pack_size_l = rec.product_tmpl_id.case_pack_size_l
+    #         rec.qty_per_ctn = rec.product_tmpl_id.qty_per_ctn
 
     def _inverse_inventory(self):
         return True
